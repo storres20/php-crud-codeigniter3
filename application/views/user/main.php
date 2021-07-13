495d987866f0415df97ceefa69666ed995d5ea0b
storres20 <sanchotorres777@gmail.com>
2021-07-13 17:40:18 -0500

    se actualizo estilos con Bootstrap 5 e iconos

--- application/views/user/main.php
+++ application/views/user/main.php
@@ -6,7 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
+    
+    <!-- Bootstrap 5 CSS - start -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap 5 CSS - end -->
+    
+    <!-- Bootstrap 5 Icon - start -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Bootstrap 5 Icon - end -->
+    
     <!-- Font Roboto CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <style>body{font-family:"Roboto" !important;}</style>
@@ -17,17 +26,20 @@
 
     <div class="container">
 
-        <h1 class="mt-5">Lista de usuarios</h1>
+        <h1 class="mt-5"><b>Lista de usuarios</b></h1>
         <div class="text-right">
-        <a href="<?php echo base_url();?>nuevo-usuario" class="btn btn-info"><ion-icon name="add"></ion-icon></a> 
+        <!-- <a href="<?php echo base_url();?>nuevo-usuario" class="btn btn-info"><ion-icon name="add"></ion-icon></a> -->
+        <a href="<?php echo base_url();?>nuevo-usuario" class="btn btn-info mb-3"><b>New </b><i class="bi bi-plus-circle-fill"></i></a>
         </div>
-        <table class="table mt-4">
-            <thead class="thead-light">
+        
+        <div class="table-responsive">
+        <table class="table table-striped table-hover align-middle">
+            <thead class="table-primary">
                 <tr>
                 <th scope="col">N°</th>
                 <th scope="col">Nombre completo</th>
                 <th scope="col">Email</th>
-                <th scope="col"></th>
+                <th scope="col">Acciones</th>
                 </tr>
             </thead>
             <tbody>
@@ -37,21 +49,26 @@
                     <td><?php echo $value->full_name; ?></td>
                     <td><?php echo $value->email; ?></td>
                     <td>
-                        <a href="<?php echo base_url();?>usuario/<?php echo $value->id; ?>" class="btn btn-primary"><ion-icon name="pencil"></ion-icon></a> 
-                        <a href="<?php echo base_url();?>usuario/delete/<?php echo $value->id; ?>" class="btn btn-danger"><ion-icon name="remove"></ion-icon></a>
+                        <a href="<?php echo base_url();?>usuario/<?php echo $value->id; ?>" class="btn btn-warning"><i class="bi bi-pencil-fill"></i></a> 
+                        <a href="<?php echo base_url();?>usuario/delete/<?php echo $value->id; ?>" class="btn btn-danger"><i class="bi bi-trash-fill"></i></a>
                     </td>
                 </tr>
                 <?php endforeach; ?>
                 
             </tbody>
         </table>
+        </div>
     </div>
 
-    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    <!-- <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script> -->
     
     <!-- Sweet Alert 2 - start -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- Sweet Alert 2 - end -->
+    
+    <!-- Bootstrap 5 Javascript - start -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Bootstrap 5 Javascript - start -->
     
     <script>
         console.log("<?php echo $this->session->flashdata("success"); ?>");
