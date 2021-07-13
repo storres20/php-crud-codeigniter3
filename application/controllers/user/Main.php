c23284ffdbfbbd0aa893cabc97605add207ab899
storres20 <sanchotorres777@gmail.com>
2021-07-12 19:57:52 -0500

    se agrega funcion constructor

--- application/controllers/user/Main.php
+++ application/controllers/user/Main.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+	public function __construct() // esta funcion cargar antes que el INDEX
+        {
+            parent::__construct();
+            $this->load->model("User_model");
+        }
+
 	public function index()
 	{
 		$this->load->view('user/main');
