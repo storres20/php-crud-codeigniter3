ef4a2e37a5b3697004d4b572b74a3fd5dca204ca
storres20 <sanchotorres777@gmail.com>
2021-07-13 13:51:37 -0500

    avances de Edit - se carga los datos en los input text en nueva pagina

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -8,4 +8,19 @@ class User_model extends CI_Model {
 	    $this->db->query("ALTER TABLE user AUTO_INCREMENT 1");
 	    $this->db->insert("user", $data);
 	}
+	
+	public function getUsers(){
+		$this->db->select("*");
+		$this->db->from("user");
+		$results = $this->db->get();
+		return $results->result();
+	}
+	
+	public function getUser($id){
+		$this->db->select("u.id, u.full_name, u.email");
+		$this->db->from("user u");
+		$this->db->where("u.id", $id);
+		$result = $this->db->get();
+		return $result->row();
+	}
 }
